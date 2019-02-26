9aa3622c0edce46ea407723fce6b9f9d9aeed6ae
J A <jarata@gmail.com>
2019-02-26 10:33:11 -0800

    roles/:id GET roles/ POST

--- roles/roles-router.js
+++ roles/roles-router.js
@@ -24,12 +24,53 @@ router.get('/', (req, res) => {
 
 router.get('/:id', (req, res) => {
   // retrieve a role by id
-  res.send('Write code to retrieve a role by id');
+  db('roles')
+      .where({id: req.params.id})
+      .then(role => {
+          res.status(200).json(role)
+      })
+      .catch(err => {
+          res.status(500).json(err)
+      })
 });
+
+// Student Solution
+// router.get('/:id', async (req, res) => {
+//     const id = req.params.id
+//     try {
+//         const role = await db('roles')
+//             .where({id})
+//         return res.status(200).json(role)
+//     } catch(error) {
+//         console.log(error)
+//         res.status(500).json(error)
+//     }
+//     // db('roles')
+//     //   .where({id})
+//     //   .then(role => {
+//     //     res.status(200).json(role)
+//     //   })
+//     //   .catch(error => {
+//     //     res.status(500).json(error)
+//     //   })
+// });
 
 router.post('/', (req, res) => {
   // add a role to the database
-  res.send('Write code to add a role');
+  db('roles')
+      .insert(req.body)
+      .then(ids => {
+          const [id] = ids;
+          db('roles')
+              .where({ id })
+              .first()
+              .then(role => {
+                  res.status(200).json(role);
+              })
+      })
+      .catch(err => {
+          res.status(500).json(err)
+      })
 });
 
 router.put('/:id', (req, res) => {
